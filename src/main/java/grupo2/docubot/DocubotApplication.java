ecacda80bd6f148f825afc11f7cfb7455d525430
zoe <baiz.zoe.luana@gmail.com>
2026-04-20 21:40:57 -0300

    first commit

---
+++ src/main/java/grupo2/docubot/DocubotApplication.java
@@ -0,0 +1,13 @@
+package grupo2.docubot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocubotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocubotApplication.class, args);
+	}
+
+}
