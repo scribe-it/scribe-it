8c31ac89c7151bc3a71b189f64236945a2c42320
zoe <baiz.zoe.luana@gmail.com>
2026-04-23 20:49:32 -0300

    inicializacion del proyecto con spring initializr

--- src/main/java/grupo2/docubot/DocubotApplication.java
+++ src/main/java/grupo2/docubot/DocubotApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DocubotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DocubotApplication.class, args);
+		//hola soy un commit
 	}
-
 }
