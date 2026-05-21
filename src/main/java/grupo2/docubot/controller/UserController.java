5d5701b0bf3670c3f33eecda7dbd42e5032f11c2
zoe-baiz <baiz.zoe.luana@gmail.com>
2026-05-21 20:51:07 -0300

    feat:added UserController

---
+++ src/main/java/grupo2/docubot/controller/UserController.java
@@ -0,0 +1,8 @@
+package grupo2.docubot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    //add post updateUser (by id)
+}
