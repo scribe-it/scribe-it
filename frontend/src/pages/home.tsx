8eb8a90430e807b34f2a572eebd38d29f53887a3
GermanEH <germanezequielhorianski@gmail.com>
2026-06-03 20:33:21 -0300

    feat(chat): connect getAll chats

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,11 +1,22 @@
 import { ChatWindow } from "@/components/chat-shadcn"
 import Layout from "@/components/layout"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { useState } from "react";
+import RegisterUser from "./register-user";
+
+const queryClient = new QueryClient();
 
 const Home = () => {
+
+  const [chatId, setChatId] = useState(0);
+  const [view, setView] = useState<"chat" | "register">("chat");
+  
   return (
-    <Layout>
-      <ChatWindow />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout setChatId={setChatId} setView={setView}>
+        {view === "chat" ? <ChatWindow chatId={chatId} /> : <RegisterUser />}
+      </Layout>
+    </QueryClientProvider>
   )
 }
 
