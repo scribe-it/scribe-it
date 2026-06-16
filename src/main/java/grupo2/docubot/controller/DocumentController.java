d9d1770f74aa9b97f612a33f4fed4bec5177ad6c
GermanEH <germanezequielhorianski@gmail.com>
2026-06-15 23:07:31 -0300

    feat(document): add controller

--- src/main/java/grupo2/docubot/controller/DocumentController.java
+++ src/main/java/grupo2/docubot/controller/DocumentController.java
@@ -1,4 +1,37 @@
 package grupo2.docubot.controller;
 
+import grupo2.docubot.dto.request.DocumentRequestDto;
+import grupo2.docubot.dto.response.DocumentResponseDto;
+import grupo2.docubot.models.UseCase;
+import grupo2.docubot.services.DocumentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/document")
+@RequiredArgsConstructor
 public class DocumentController {
+
+    private final DocumentService documentService;
+
+    @GetMapping
+    public ResponseEntity<List<DocumentResponseDto>> getAll() {
+        return ResponseEntity.ok().body(documentService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<DocumentResponseDto> create(@RequestBody DocumentRequestDto documentRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(documentService.create(documentRequestDto));
+    }
+
+    @PatchMapping("/{documentId}")
+    public ResponseEntity<DocumentResponseDto> addUseCase(@PathVariable Long documentId, @RequestBody UseCase useCase) {
+        return ResponseEntity.ok().body(documentService.addUseCase(documentId, useCase));
+    }
+
+
 }
