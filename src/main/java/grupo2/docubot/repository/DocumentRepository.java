40b75b2fa16d9f39c60cc6b274bca6735722f099
GermanEH <germanezequielhorianski@gmail.com>
2026-06-17 17:25:10 -0300

    feat(document): add findAllByPublishedFalse and findAllByPublishedTrue documents query methods

--- src/main/java/grupo2/docubot/repository/DocumentRepository.java
+++ src/main/java/grupo2/docubot/repository/DocumentRepository.java
@@ -3,5 +3,11 @@ package grupo2.docubot.repository;
 import grupo2.docubot.models.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+
+    List<Document> findAllByPublishedFalse();
+
+    List<Document> findAllByPublishedTrue();
 }
