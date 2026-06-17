41f6620227fd6a1fff89df48ac60539151c52656
zoe-baiz <baiz.zoe.luana@gmail.com>
2026-06-17 20:01:20 -0300

    mensaje

--- src/main/java/grupo2/docubot/DocubotApplication.java
+++ src/main/java/grupo2/docubot/DocubotApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class DocubotApplication {
 	public static void main(String[] args) {
+		//hola
 		SpringApplication.run(DocubotApplication.class, args);
 	}
 }
