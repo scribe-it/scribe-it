80b8abfc23dd2091bbd5a5a62d0cf11ac0501f19
zoe-baiz <baiz.zoe.luana@gmail.com>
2026-06-18 18:25:47 -0300

    added admin permissions

--- src/main/java/grupo2/docubot/controller/DocumentController.java
+++ src/main/java/grupo2/docubot/controller/DocumentController.java
@@ -12,6 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/document")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ANALISTA')")
 public class DocumentController {
 
     private final DocumentService documentService;
--- src/main/java/grupo2/docubot/controller/UseCaseController.java
+++ src/main/java/grupo2/docubot/controller/UseCaseController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/use_cases")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ANALISTA')")
 public class UseCaseController {
 
     private final UseCaseService useCaseService;
