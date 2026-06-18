5c89753ef603873c68648354a4fb1902ea9ebf3b
zoe-baiz <baiz.zoe.luana@gmail.com>
2026-06-18 17:54:13 -0300

    added import for value annotation in JwtService

--- src/main/java/grupo2/docubot/services/JwtService.java
+++ src/main/java/grupo2/docubot/services/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
